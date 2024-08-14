5a8f4c90f4ae939e1fb23f73481b9ad729615ebd
ypsitau <ypsitau@nifty.com>
2024-08-14 21:15:35 +0900

    implementing pico-fastest

--- pio-fastest/pio-fastest.cpp
+++ pio-fastest/pio-fastest.cpp
@@ -8,13 +8,10 @@ int main()
 {
 	uint32_t freqPeri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI) * 1000;
 	stdio_init_all();
-	PIO pio = pio0;
-	uint fastest_program_offset = pio_add_program(pio, &fastest_program);
+	PIOSM_fastest pioSm(pio0, 0);
 	do {
-		uint pin = 14;
-		uint idxSm = 0;
-		fastest_program_init(pio, idxSm, fastest_program_offset, pin);
-		pio_sm_set_enabled(pio, idxSm, true);
+		pioSm.Init(14);
+		pio_sm_set_enabled(pioSm.GetPIO(), pioSm.GetIdxSm(), true);
 	} while (0);
 	for (;;) ;
 }
