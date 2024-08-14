98913684867fab1c08f94e0345435227a77bad3e
ypsitau <ypsitau@nifty.com>
2024-08-14 16:00:59 +0900

    implementing pio-blink

--- pio-blink/pio-blink.c
+++ pio-blink/pio-blink.c
@@ -1,40 +1,41 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
-
+#include <pico/stdlib.h>
+#include <hardware/pio.h>
+#include <hardware/clocks.h>
 #include "blink.pio.h"
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
-
 
 int main()
 {
-    stdio_init_all();
-
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+	uint32_t freqPeri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI) * 1000;
+	stdio_init_all();
+	PIO pio = pio0;
+	uint offsetProgram = pio_add_program(pio, &blink_program);
+	do {
+		uint pin = 6;
+		uint idxSm = 0;
+		uint freq = 3;
+		blink_program_init(pio, idxSm, offsetProgram, pin);
+		pio_sm_set_enabled(pio, idxSm, true);
+		pio->txf[idxSm] = (freqPeri / (2 * freq)) - 3;
+	} while (0);
+	do {
+		uint pin = 7;
+		uint idxSm = 1;
+		uint freq = 4;
+		blink_program_init(pio, idxSm, offsetProgram, pin);
+		pio_sm_set_enabled(pio, idxSm, true);
+		pio->txf[idxSm] = (freqPeri / (2 * freq)) - 3;
+	} while (0);
+	do {
+		uint pin = 8;
+		uint idxSm = 2;
+		uint freq = 1;
+		blink_program_init(pio, idxSm, offsetProgram, pin);
+		pio_sm_set_enabled(pio, idxSm, true);
+		pio->txf[idxSm] = (freqPeri / (2 * freq)) - 3;
+	} while (0);
+	while (true) {
+		printf("Hello, world!\n");
+		sleep_ms(1000);
+	}
 }
