2af7edd57cfd5f08c3cfd5bdff796ec225cc026e
ypsitau <ypsitau@nifty.com>
2024-08-15 15:34:17 +0900

    implementing pio test

--- pio-blink/pio-blink.c
+++ pio-blink/pio-blink.c
@@ -6,33 +6,29 @@
 
 int main()
 {
-	uint32_t freqPeri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI) * 1000;
 	stdio_init_all();
 	PIO pio = pio0;
 	uint offsetProgram = pio_add_program(pio, &blink_program);
 	do {
-		uint pin = 6;
+		uint pin = 10;
 		uint idxSm = 0;
 		uint freq = 3;
 		blink_program_init(pio, idxSm, offsetProgram, pin);
-		pio_sm_set_enabled(pio, idxSm, true);
-		pio->txf[idxSm] = (freqPeri / (2 * freq)) - 3;
+		blink_start(pio, idxSm, freq);
 	} while (0);
 	do {
-		uint pin = 7;
+		uint pin = 11;
 		uint idxSm = 1;
 		uint freq = 4;
 		blink_program_init(pio, idxSm, offsetProgram, pin);
-		pio_sm_set_enabled(pio, idxSm, true);
-		pio->txf[idxSm] = (freqPeri / (2 * freq)) - 3;
+		blink_start(pio, idxSm, freq);
 	} while (0);
 	do {
-		uint pin = 8;
+		uint pin = 12;
 		uint idxSm = 2;
 		uint freq = 1;
 		blink_program_init(pio, idxSm, offsetProgram, pin);
-		pio_sm_set_enabled(pio, idxSm, true);
-		pio->txf[idxSm] = (freqPeri / (2 * freq)) - 3;
+		blink_start(pio, idxSm, freq);
 	} while (0);
 	while (true) {
 		printf("Hello, world!\n");
