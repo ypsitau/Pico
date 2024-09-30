ae3be90bbe581b57798f7e575e3f026bc279393b
ypsitau <ypsitau@nifty.com>
2024-09-30 15:45:52 +0900

    implementing SSD1306 class

--- mylib/SSD1306/test-SSD1306.cpp
+++ mylib/SSD1306/test-SSD1306.cpp
@@ -100,6 +100,60 @@ void render(uint8_t *buf)
     SSD1306_send_buf(buf, SSD1306_BUF_LEN);
 }
 
+static void SetPixel(uint8_t *buf, int x,int y, bool on)
+{
+    assert(x >= 0 && x < SSD1306_WIDTH && y >=0 && y < SSD1306_HEIGHT);
+
+    // The calculation to determine the correct bit to set depends on which address
+    // mode we are in. This code assumes horizontal
+
+    // The video ram on the SSD1306 is split up in to 8 rows, one bit per pixel.
+    // Each row is 128 long by 8 pixels high, each byte vertically arranged, so byte 0 is x=0, y=0->7,
+    // byte 1 is x = 1, y=0->7 etc
+
+    // This code could be optimised, but is like this for clarity. The compiler
+    // should do a half decent job optimising it anyway.
+
+    const int BytesPerRow = SSD1306_WIDTH ; // x pixels, 1bpp, but each row is 8 pixel high, so (x / 8) * 8
+
+    int byte_idx = (y / 8) * BytesPerRow + x;
+    uint8_t byte = buf[byte_idx];
+
+    if (on)
+        byte |=  1 << (y % 8);
+    else
+        byte &= ~(1 << (y % 8));
+
+    buf[byte_idx] = byte;
+}
+
+static void DrawLine(uint8_t *buf, int x0, int y0, int x1, int y1, bool on)
+{
+
+    int dx =  abs(x1-x0);
+    int sx = x0<x1 ? 1 : -1;
+    int dy = -abs(y1-y0);
+    int sy = y0<y1 ? 1 : -1;
+    int err = dx+dy;
+    int e2;
+
+    while (true) {
+        SetPixel(buf, x0, y0, on);
+        if (x0 == x1 && y0 == y1)
+            break;
+        e2 = 2*err;
+
+        if (e2 >= dy) {
+            err += dy;
+            x0 += sx;
+        }
+        if (e2 <= dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
 int main()
 {
 	::stdio_init_all();
@@ -160,4 +214,7 @@ int main()
 		SSD1306_send_cmd(SSD1306_SET_ENTIRE_ON); // go back to following RAM for pixel state
 		sleep_ms(500);
     }
+	DrawLine(buf, 0, 0, 100, 30, true);
+	render(buf);
+	for (;;) ;
 }
