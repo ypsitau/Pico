a0544177c437daeb5cefe7a0f655572be41dca43
ypsitau <ypsitau@nifty.com>
2024-10-25 17:00:59 +0900

    implementing testapp for SSD1306

--- pico-jxglib/SSD1306/testapp/testapp.cpp
+++ pico-jxglib/SSD1306/testapp/testapp.cpp
@@ -1,8 +1,10 @@
+// To generate font files:
+//   gurax ../../Font/script/GenerateFont.gura -o Font shinonome12,shinonome14,shinonome16 *.cpp
 #include <stdio.h>
 #include <jxglib/SSD1306.h>
-#include "jxglib/Font/shinonome12.h"
-#include "jxglib/Font/shinonome14.h"
-#include "jxglib/Font/shinonome16.h"
+#include "Font/shinonome12.h"
+#include "Font/shinonome14.h"
+#include "Font/shinonome16.h"
 
 void SetupStage(SSD1306& oled, int iCase)
 {
@@ -26,11 +28,11 @@ int main()
 	oled.SetFontScale(1, 1);
 	oled.Clear();
 	oled.SetFont(Font::shinonome12);
-	oled.DrawString(0, 0, "ABCDEFG");
+	oled.DrawString(0, 0, "本日は晴天なり");
 	oled.SetFont(Font::shinonome14);
-	oled.DrawString(0, 16, "ABCDEFG");
+	oled.DrawString(0, 16, "本日は晴天なり");
 	oled.SetFont(Font::shinonome16);
-	oled.DrawString(0, 32, "ABCDEFG");
+	oled.DrawString(0, 32, "本日は晴天なり");
 	oled.Refresh();
 #if 0
 #if 1
